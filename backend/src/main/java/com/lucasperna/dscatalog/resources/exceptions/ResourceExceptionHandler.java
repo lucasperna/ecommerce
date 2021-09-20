6271e5508f56a3957dc0cdee68d792c3be2fb326
Lucas Perna <lucasmalamao@gmail.com>
2021-09-20 16:20:16 -0300

    Product entity (many-to-many)

---
+++ backend/src/main/java/com/lucasperna/dscatalog/resources/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,41 @@
+package com.lucasperna.dscatalog.resources.exceptions;
+
+import java.time.Instant;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.lucasperna.dscatalog.services.exceptions.DatabaseException;
+import com.lucasperna.dscatalog.services.exceptions.ResourceNotFoundException;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+
+	@ExceptionHandler(ResourceNotFoundException.class)
+	public ResponseEntity<StandardError> entityNotFound(ResourceNotFoundException e, HttpServletRequest request) {
+		HttpStatus httpStatus = HttpStatus.NOT_FOUND;
+		StandardError err = new StandardError();
+		err.setTimestamp(Instant.now());
+		err.setStatus(httpStatus.value());
+		err.setError("Resource not found.");
+		err.setMessage(e.getMessage());
+		err.setPath(request.getRequestURI());
+		return ResponseEntity.status(httpStatus).body(err);
+	}
+	
+	@ExceptionHandler(DatabaseException.class)
+	public ResponseEntity<StandardError> database(DatabaseException e, HttpServletRequest request) {
+		HttpStatus httpStatus = HttpStatus.BAD_REQUEST;
+		StandardError err = new StandardError();
+		err.setTimestamp(Instant.now());
+		err.setStatus(httpStatus.value());
+		err.setError("Database exception.");
+		err.setMessage(e.getMessage());
+		err.setPath(request.getRequestURI());
+		return ResponseEntity.status(httpStatus).body(err);
+	}
+}
